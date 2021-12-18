41b26b7b209a6bc22e7321d1d5a7b63172749087
Gabriel Magdaleno <gmagdaleno1981@gmail.com>
2021-12-18 16:58:21 +0100

    nivel 2 creado

--- main.ts
+++ main.ts
@@ -1,30 +1,163 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite, location) {
+    tiles.setTileAt(location, sprites.dungeon.hazardLava0)
+    game.over(true, effects.melt)
+    music.wawawawaa.play()
+})
+function nivel_2 () {
+    treasure.destroy()
+    treasure1.destroy()
+    treasure2.destroy()
+    treasure3.destroy()
+    tiles.setTilemap(tilemap`level2`)
+    colorFondo(randint(0, 14))
+    goodGuy.ay = 200
+    badGuy.setPosition(1024, 8)
+    level = 2
+}
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
+    tiles.setTileAt(location, img`
+        . b b b b b b b b b b b b b b . 
+        b e 4 4 4 4 4 4 4 4 4 4 4 4 4 b 
+        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+        b e e 4 4 4 4 4 4 4 4 4 4 e e b 
+        b b b b b b b d d b b b b b b b 
+        . b b b b b b c c b b b b b b . 
+        b c c c c c b c c b c c c c c b 
+        b c c c c c c b b c c c c c c b 
+        b c c c c c c c c c c c c c c b 
+        b c c c c c c c c c c c c c c b 
+        b b b b b b b b b b b b b b b b 
+        b e e e e e e e e e e e e e e b 
+        b e e e e e e e e e e e e e e b 
+        b c e e e e e e e e e e e e c b 
+        b b b b b b b b b b b b b b b b 
+        . b b . . . . . . . . . . b b . 
+        `)
+    info.changeScoreBy(1)
+    music.wawawawaa.play()
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    game.over(false, effects.dissolve)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardWater, function (sprite, location) {
+    tiles.setTileAt(location, sprites.dungeon.hazardWater)
+    game.over(true, effects.bubbles)
+    music.wawawawaa.play()
+})
+function colorFondo (codigoColor: number) {
+    if (codigoColor == 0) {
+        scene.setBackgroundColor(0)
+    }
+    if (codigoColor == 1) {
+        scene.setBackgroundColor(1)
+    }
+    if (codigoColor == 2) {
+        scene.setBackgroundColor(2)
+    }
+    if (codigoColor == 3) {
+        scene.setBackgroundColor(3)
+    }
+    if (codigoColor == 4) {
+        scene.setBackgroundColor(4)
+    }
+    if (codigoColor == 5) {
+        scene.setBackgroundColor(5)
+    }
+    if (codigoColor == 6) {
+        scene.setBackgroundColor(6)
+    }
+    if (codigoColor == 7) {
+        scene.setBackgroundColor(7)
+    }
+    if (codigoColor == 8) {
+        scene.setBackgroundColor(8)
+    }
+    if (codigoColor == 9) {
+        scene.setBackgroundColor(9)
+    }
+    if (codigoColor == 10) {
+        scene.setBackgroundColor(10)
+    }
+    if (codigoColor == 11) {
+        scene.setBackgroundColor(11)
+    }
+    if (codigoColor == 12) {
+        scene.setBackgroundColor(12)
+    }
+    if (codigoColor == 13) {
+        scene.setBackgroundColor(13)
+    }
+    if (codigoColor == 14) {
+        scene.setBackgroundColor(14)
+    }
+}
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     if (goodGuy.overlapsWith(treasure)) {
-        info.setScore(1)
+        info.changeScoreBy(1)
         treasure.destroy()
         music.baDing.play()
     }
     if (goodGuy.overlapsWith(treasure1)) {
-        info.setScore(1)
+        info.changeScoreBy(1)
         treasure1.destroy()
         music.baDing.play()
     }
     if (goodGuy.overlapsWith(treasure2)) {
-        info.setScore(1)
+        info.changeScoreBy(1)
         treasure2.destroy()
         music.baDing.play()
     }
     if (goodGuy.overlapsWith(treasure3)) {
-        info.setScore(1)
+        info.changeScoreBy(1)
         treasure3.destroy()
         music.baDing.play()
     }
 })
+let level = 0
+let badGuy: Sprite = null
 let goodGuy: Sprite = null
 let treasure3: Sprite = null
 let treasure2: Sprite = null
 let treasure1: Sprite = null
 let treasure: Sprite = null
+game.splash("Come - Come", "Autor: Gabriel Magdaleno")
+game.setDialogTextColor(4)
+game.setDialogFrame(img`
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    `)
+game.setDialogCursor(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 6 6 6 6 . . . . . . 
+    . . . . 6 6 6 5 5 6 6 6 . . . . 
+    . . . 7 7 7 7 6 6 6 6 6 6 . . . 
+    . . 6 7 7 7 7 8 8 8 1 1 6 6 . . 
+    . . 7 7 7 7 7 8 8 8 1 1 5 6 . . 
+    . 6 7 7 7 7 8 8 8 8 8 5 5 6 6 . 
+    . 6 7 7 7 8 8 8 6 6 6 6 5 6 6 . 
+    . 6 6 7 7 8 8 6 6 6 6 6 6 6 6 . 
+    . 6 8 7 7 8 8 6 6 6 6 6 6 6 6 . 
+    . . 6 8 7 7 8 6 6 6 6 6 8 6 . . 
+    . . 6 8 8 7 8 8 6 6 6 8 6 6 . . 
+    . . . 6 8 8 8 8 8 8 8 8 6 . . . 
+    . . . . 6 6 8 8 8 8 6 6 . . . . 
+    . . . . . . 6 6 6 6 . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `)
+game.showLongText("Tienes que recoger los 4 tesoros e impedir que el malo te detenga", DialogLayout.Center)
 info.setScore(0)
 tiles.setTilemap(tilemap`level1`)
 treasure = sprites.create(img`
@@ -64,7 +197,7 @@ treasure1 = sprites.create(img`
     . . . . . . 6 6 6 6 . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Food)
-treasure1.setPosition(224, 48)
+treasure1.setPosition(232, 56)
 treasure2 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . 4 4 4 4 . . . . . . 
@@ -83,7 +216,7 @@ treasure2 = sprites.create(img`
     . . . . . . 4 4 4 4 . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Food)
-treasure2.setPosition(32, 112)
+treasure2.setPosition(40, 120)
 treasure3 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . 4 4 4 4 . . . . . . 
@@ -102,7 +235,7 @@ treasure3 = sprites.create(img`
     . . . . . . 4 4 4 4 . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Food)
-treasure3.setPosition(160, 208)
+treasure3.setPosition(168, 216)
 let level1 = sprites.create(img`
     6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
     6 9 9 9 6 6 9 9 9 9 6 6 9 9 9 6 
@@ -121,7 +254,7 @@ let level1 = sprites.create(img`
     6 9 9 9 6 6 9 9 9 9 6 6 9 9 9 6 
     6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
     `, SpriteKind.Food)
-level1.setPosition(80, 112)
+level1.setPosition(88, 120)
 goodGuy = sprites.create(img`
     8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
     8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
@@ -141,7 +274,7 @@ goodGuy = sprites.create(img`
     8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
     `, SpriteKind.Player)
 goodGuy.setPosition(8, 8)
-let badGuy = sprites.create(img`
+badGuy = sprites.create(img`
     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
     2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
     2 2 5 5 5 5 5 2 2 5 5 5 5 5 2 2 
@@ -163,5 +296,17 @@ badGuy.setPosition(150, 110)
 badGuy.follow(goodGuy, 25)
 controller.moveSprite(goodGuy, 100, 100)
 scene.cameraFollowSprite(goodGuy)
-let level = 1
+level = 1
 let direction = 0
+forever(function () {
+    if (info.score() == 4 && goodGuy.overlapsWith(level1)) {
+        info.changeScoreBy(10)
+        level1.destroy()
+        music.magicWand.play()
+        nivel_2()
+    }
+    if (info.score() == 28) {
+        game.over(true, effects.confetti)
+        music.magicWand.play()
+    }
+})
